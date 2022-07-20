52c4ce9d5bf098b41e835e55821030ca3aa1af96
divyeshio <79130336+divyeshio@users.noreply.github.com>
2022-07-20 14:29:39 +0530

    Update api name

--- src/SmartLearning.Web/Program.cs
+++ src/SmartLearning.Web/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddControllersWithViews(op =>
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Smart Learning API", Version = "v1" });
     c.EnableAnnotations();
 });
 
